7bec700868a63b2cd292b0f20ab5e8863d0c31bc
Duane Grell <duanegrell@gmail.com>
2023-03-23 11:37:48 -0400

    in case comp crashes

--- server/models.py
+++ server/models.py
@@ -10,4 +10,55 @@ metadata = MetaData(naming_convention={
 
 db = SQLAlchemy(metadata=metadata)
 
+class Pizza(db.Model, SerializerMixin):
+    __tablename__ = "pizzas"
+
+    serialize_rules = ('-restaurant_pizzas.pizza', '-restauants.pizzas')
+
+    id = db.Column(db.Integer, primary_key = True)
+
+    name = db.Column(db.String)
+    ingredients = db.Column(db.String)
+
+    created_at = db.Column(db.DateTime, server_default = db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate = db.func.now())
+
+    restaurant_pizzas = db.relationship('RestaurantPizza', backref = 'pizza')
+    restaurants = association_proxy('restaurant_pizzas', 'restaurant')
+
+class Restaurant(db.Model, SerializerMixin):
+    __tablename__ = "restaurants"
+
+    serialize_rules = ('-restaurant_pizzas.restaurant', '-pizzas.restaurants')
+
+    id = db.Column(db.Integer, primary_key = True)
+
+    name = db.Column(db.String)
+    address = db.Column(db.String)
+
+    restaurant_pizzas = db.relationship('RestaurantPizza', backref = 'restaurant')
+    restaurants = association_proxy('restaurant_pizzas', 'pizza')
+
+class RestaurantPizza(db.Model, SerializerMixin):
+    __tablename__ = "restaurant_pizzas"
+
+    id = db.Column(db.Integer, primary_key = True)
+
+    pizza_id = db.Column(db.Integer, db.ForeignKey('pizzas.id'))
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'))
+
+    price = db.Column(db.Integer)
+
+    created_at = db.Column(db.DateTime, server_default = db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate = db.func.now())
+
+    @validates('price')
+    def validates_price(self, key, price):
+        if price < 1 or price > 30:
+            raise ValueError ("must have a price between 1 and 30")
+        return price
+
+
+
+
 # Add models here
